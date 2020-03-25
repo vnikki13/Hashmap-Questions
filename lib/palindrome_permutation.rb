9306e882f18502237936d6b5afd9a16126308dd1
Nikki Vaughan <vnikki13@gmail.com>
2020-03-25 12:21:51 -0700

    Finished plaindrome_permutation? method with passing tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,12 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return true if string.length == 0
+  hash = Hash.new(0)
+
+  string.each_char do |ele|
+    hash[ele] += 1
+  end
+
+  hash.values.one?(1) ? (return true) : (return false)
+  # raise NotImplementedError, "palindrome_permutation? not implemented"
 end
