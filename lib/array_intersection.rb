787c5bc57b631bc62423ba7c4843d40c90992a42
Nikki Vaughan <vnikki13@gmail.com>
2020-03-25 11:20:07 -0700

    Finished intersection method with passing tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,15 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  combined = Hash.new(0)
+  multiple = []
+
+  list1.each do |ele|
+    combined[ele] += 1
+  end
+
+  list2.each do |ele|
+    combined[ele] += 1
+    multiple << ele if combined[ele] == 2
+  end
+  return multiple
+  # raise NotImplementedError, "Intersection not implemented"
 end
